66ca32dedb83e797ef89d662833a8c835f772960
Trua Nguyen <nguyendangthanh1999hn@gmail.com>
2020-10-14 13:40:13 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,174 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ChatMessage{
+  String date, msg, id;
+  ChatMessage({this.date, this.msg, this.id});
+}
+
+class MyApp extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Messenger',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp>{
+  SocketIO socketIO;
+  TextEditingController inputMsg;
+  String randomID = '';
+  int conn = 0;
+  List<ChatMessage> listChat = new List<ChatMessage>();
+
+  @override
+  void initState() {
+    var rng = new Random();
+    randomID = 'USER'+rng.nextInt(10000).toString();
+    inputMsg = new TextEditingController();
+    initSocketIO();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  initSocketIO(){
+    //update the domain before using
+    socketIO = SocketIOManager().createSocketIO("http://192.168.1.27:3002", "/room", query: "userId=100", socketStatusCallback: _socketStatus);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    //call init socket before doing anything
+    socketIO.init();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    //subscribe event
+    socketIO.subscribe("new_message", _getMessage);
+    socketIO.subscribe("socket_connections", _getConnections);
 
-  final String title;
+    //connect socket
+    socketIO.connect();
+  }
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  _socketStatus(dynamic data) {
+    print("Socket status: " + data);
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void _sendMessage() async {
+    if (socketIO != null) {
+      String jsonData = '{msg: "'+inputMsg.text+'",id: '+randomID+'}';
+      socketIO.sendMessage("send message", jsonData);
+      inputMsg.clear();
+    }
+  }
 
-  void _incrementCounter() {
+  void _getMessage(dynamic data){
+    print(data);
+    Map<String,dynamic> map = new Map<String,dynamic>();
+    map = json.decode(data);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      listChat.add(new ChatMessage(date: map['date'], msg: map['msg'], id: map['id']));
     });
   }
 
+  void _getConnections(dynamic data){
+    print(data);
+    Map<String,dynamic> map = new Map<String, dynamic>();
+    map = json.decode(data);
+    setState(()=>conn = map['socket']);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      resizeToAvoidBottomPadding: true,
+      resizeToAvoidBottomInset: true,
+      appBar: new AppBar(
+        title: new Text('Chat Messaging with Socket.io'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: new SingleChildScrollView(
+        padding: new EdgeInsets.all(16.0),
+        child: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  new Text('Your ID: '+randomID),
+                  new Text('Total Connections: '+conn.toString())
+                ],
+              ),
+              new ListView.builder(
+                physics: ScrollPhysics(),
+                shrinkWrap: true,
+                reverse: true,
+                itemCount: listChat.length,
+                itemBuilder: ((ctx, idx){
+                  return _msgContainer(listChat[idx]);
+                }),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+                flex: 5,
+                child: new Padding(
+                  padding: new EdgeInsets.all(5.0),
+                  child: new TextField(
+                    controller: inputMsg,
+                    decoration: new InputDecoration.collapsed(hintText: 'Send a Message'),
+                  ),
+                )
+            ),
+            Expanded(
+              flex: 1,
+              child: new FlatButton.icon(onPressed: _sendMessage, icon: Icon(Icons.send), label: new Text('')),
+            )
+          ],
+        ),
+        elevation: 9.0,
+        shape: CircularNotchedRectangle(),
+        color: Colors.white,
+        notchMargin: 8.0,
+      ),
     );
+  }
+
+  Widget _msgContainer(ChatMessage chat){
+    if (chat.id != randomID){
+      return new Container(
+        decoration: new BoxDecoration(border: new Border.all(color: Colors.black), borderRadius: new BorderRadius.circular(10.0)),
+        margin: new EdgeInsets.all(3.0),
+        padding: new EdgeInsets.only(top: 16.0, bottom: 16.0, right: 8.0, left: 8.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(chat.id+'('+chat.date+')', style: new TextStyle(color: Colors.grey),),
+            new Text(chat.msg, )
+          ],
+        ),
+      );
+    }else{
+      return new Container(
+        decoration: new BoxDecoration(border: new Border.all(color: Colors.black), borderRadius: new BorderRadius.circular(10.0)),
+        margin: new EdgeInsets.all(3.0),
+        padding: new EdgeInsets.only(top: 16.0, bottom: 16.0, right: 8.0, left: 8.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text('YOU', style: new TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),),
+            new Text(chat.msg, )
+          ],
+        ),
+      );
+    }
   }
 }
